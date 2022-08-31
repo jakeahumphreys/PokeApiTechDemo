ed5300c434a5a8d1aa3beaa57aad1109e77e370e
jakeahumphreys <jakehumphreys@outlook.com>
2022-08-31 12:02:53 +0100

    Formatting

--- Cache/CacheRepository.cs
+++ Cache/CacheRepository.cs
@@ -17,7 +17,6 @@ namespace PokeApiTechDemo.Cache
     {
         public const string INSERT_COMMAND =
             @"INSERT INTO cache_entries (name, time, blob) VALUES ($name, $time, $blob);";
-
         public const string GET_COMMAND = @"SELECT * FROM cache_entries WHERE name = $name";
         public const string UPDATE_COMMAAND = @"UPDATE cache_entries SET time = $time, blob = $blob WHERE name = $name";
         
