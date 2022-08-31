5238702896126ec419cda7e55f0edf5bf74d7708
jakeahumphreys <jakehumphreys@outlook.com>
2022-08-31 14:23:26 +0100

    UI Rework

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -36,65 +36,67 @@
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.pbMainImage = new System.Windows.Forms.PictureBox();
             this.tvDetails = new System.Windows.Forms.TreeView();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.txtJson = new System.Windows.Forms.RichTextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize) (this.pbMainImage)).BeginInit();
-            this.tabPage2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstHistory
             // 
             this.lstHistory.FormattingEnabled = true;
-            this.lstHistory.Location = new System.Drawing.Point(6, 19);
+            this.lstHistory.Location = new System.Drawing.Point(7, 16);
+            this.lstHistory.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.lstHistory.Name = "lstHistory";
-            this.lstHistory.Size = new System.Drawing.Size(203, 394);
+            this.lstHistory.Size = new System.Drawing.Size(236, 121);
             this.lstHistory.TabIndex = 0;
             this.lstHistory.SelectedIndexChanged += new System.EventHandler(this.lstHistory_SelectedIndexChanged);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.lstHistory);
-            this.groupBox1.Location = new System.Drawing.Point(14, 10);
+            this.groupBox1.Location = new System.Drawing.Point(14, 75);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(215, 427);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(251, 153);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "History";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.lblStatus);
             this.groupBox2.Controls.Add(this.btnSearch);
             this.groupBox2.Controls.Add(this.txtSearch);
-            this.groupBox2.Location = new System.Drawing.Point(241, 12);
+            this.groupBox2.Location = new System.Drawing.Point(14, 11);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(550, 82);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox2.Size = new System.Drawing.Size(260, 55);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Fetch Pokémon";
             // 
             // lblStatus
             // 
-            this.lblStatus.Location = new System.Drawing.Point(16, 47);
+            this.lblStatus.Location = new System.Drawing.Point(14, 429);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(109, 15);
+            this.lblStatus.Size = new System.Drawing.Size(127, 16);
             this.lblStatus.TabIndex = 2;
             this.lblStatus.Text = "Ready...";
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(481, 24);
+            this.btnSearch.Location = new System.Drawing.Point(178, 23);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(63, 20);
+            this.btnSearch.Size = new System.Drawing.Size(74, 20);
             this.btnSearch.TabIndex = 1;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -102,98 +104,90 @@
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(16, 24);
+            this.txtSearch.Location = new System.Drawing.Point(7, 23);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(463, 20);
+            this.txtSearch.Size = new System.Drawing.Size(162, 22);
             this.txtSearch.TabIndex = 0;
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.tabControl1);
-            this.groupBox3.Location = new System.Drawing.Point(241, 118);
+            this.groupBox3.Controls.Add(this.pbMainImage);
+            this.groupBox3.Controls.Add(this.tvDetails);
+            this.groupBox3.Location = new System.Drawing.Point(286, 11);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(543, 319);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox3.Size = new System.Drawing.Size(202, 413);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Results";
             // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 18);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(521, 287);
-            this.tabControl1.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.pbMainImage);
-            this.tabPage1.Controls.Add(this.tvDetails);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(513, 261);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Details";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
             // pbMainImage
             // 
-            this.pbMainImage.Location = new System.Drawing.Point(290, 14);
+            this.pbMainImage.Location = new System.Drawing.Point(7, 19);
+            this.pbMainImage.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pbMainImage.Name = "pbMainImage";
-            this.pbMainImage.Size = new System.Drawing.Size(212, 184);
+            this.pbMainImage.Size = new System.Drawing.Size(186, 127);
             this.pbMainImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbMainImage.TabIndex = 1;
             this.pbMainImage.TabStop = false;
             // 
             // tvDetails
             // 
-            this.tvDetails.Location = new System.Drawing.Point(18, 14);
+            this.tvDetails.Location = new System.Drawing.Point(7, 153);
+            this.tvDetails.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tvDetails.Name = "tvDetails";
-            this.tvDetails.Size = new System.Drawing.Size(260, 241);
+            this.tvDetails.Size = new System.Drawing.Size(185, 254);
             this.tvDetails.TabIndex = 0;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.txtJson);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(513, 261);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "JSON";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // txtJson
             // 
-            this.txtJson.Location = new System.Drawing.Point(12, 14);
+            this.txtJson.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.txtJson.Location = new System.Drawing.Point(7, 19);
+            this.txtJson.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txtJson.Name = "txtJson";
-            this.txtJson.Size = new System.Drawing.Size(489, 218);
+            this.txtJson.Size = new System.Drawing.Size(236, 166);
             this.txtJson.TabIndex = 0;
             this.txtJson.Text = "";
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtJson);
+            this.groupBox4.Location = new System.Drawing.Point(14, 232);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox4.Size = new System.Drawing.Size(251, 192);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Raw JSON";
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(487, 452);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("SimSun", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "MainForm";
             this.Text = "PokéApi Tech Demo (.NET 4.8)";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize) (this.pbMainImage)).EndInit();
-            this.tabPage2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.GroupBox groupBox4;
 
         private System.Windows.Forms.PictureBox pbMainImage;
 
@@ -208,9 +202,6 @@
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
 
         #endregion
     }
