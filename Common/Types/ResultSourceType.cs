7b69980075f8f235c4f981a90df6b4a96fbf155a
jakeahumphreys <jakehumphreys@outlook.com>
2022-09-06 21:55:15 +0100

    Moving the main solution into its own file

--- Common/Types/ResultSourceType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PokeApiTool.Common.Types
-{
-    public static class ResultSourceType
-    {
-        public const string RESULT_API = "Api";
-        public const string RESULT_CACHE = "Cache";
-    }
-}
--- Data/ConnectionStrings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PokeApiTool.Data
-{
-    public static class ConnectionStrings
-    {
-        public const string CACHE_DATABASE = "Data Source=resultCache.sqlite;Version=3";
-    }
-}
