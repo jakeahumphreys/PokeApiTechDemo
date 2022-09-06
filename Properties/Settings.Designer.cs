3a8196ab798a37c3658bd1a22630dc861ca44e72
jakeahumphreys <jakehumphreys@outlook.com>
2022-09-06 20:44:48 +0100

    Moving towards release, setting debug to false

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace PokeApiTechDemo.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool DebugLogging {
             get {
                 return ((bool)(this["DebugLogging"]));
